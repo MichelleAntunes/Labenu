6af1bd421452c305c54a888830f8fccda1edaab3
MichelleAntunes <miichelleantuness@gmail.com>
2022-11-25 20:45:24 +0100

    Novos exercicios Strings

--- Strings/strings-exercicio/exercicio2/script.js
+++ Strings/strings-exercicio/exercicio2/script.js
@@ -8,18 +8,19 @@ const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      
 A partir dela, execute os passos abaixo:
 
 a) Remova o excesso de espaços no final da string;*/
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log ("Quantidade de caracteres da string antes da remoção dos espaços:", minhaString.length)
+
+//const minhaString2 = minhaString.trim()
 
 /*b) exiba, em um console.log() a quantidade de caracteres da string, antes e depois da remoção dos espaços;*/
 
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
-console.log (minhaString)
 
-console.log("Quantidade antes da remoção de espaços:", minhaString.length)
+const minhaString2 = minhaString.trim() //removendo espaços
 
-const minhaString2 = minhaString.trim() 
-
-console.log("Quantidade antes da remoção de espaços:", minhaString2.length)
-
+console.log ("Quantidade de caracteres da string antes da remoção dos espaços:", minhaString2.length)
 
 /*c) Substitua os traços `________` por “sticioso”.*/
 
+const novaString = minhaString2.replaceAll("________", "sticioso")
+console.log (novaString)
