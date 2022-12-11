15a13c41a489c8683d4a45899c2826da97c9800e
MichelleAntunes <miichelleantuness@gmail.com>
2022-12-11 13:37:45 +0100

    Alterações Projeto Web

--- Projeto Web/projeto-intro-web/index.js
+++ Projeto Web/projeto-intro-web/index.js
@@ -1,31 +1,46 @@
 // meu item será cidades turisticas na Alemanha
 
-const city1 = {
-  nameCity1: `Weimar`.toUpperCase(),
-  stade3: "Turíngia",
-  quantity1: Number(`65.004`),
-  capital1: false,
-  foundationYear1: 1918,
-  mainParty: "Zwiebelfest e Weimarer Weihnacht",
-};
-const city2 = {
-  nameCity2: "Dresden".toUpperCase(),
-  stade3: "Saxônia",
-  quantity2: Number(`554.649`),
-  capital2: true,
-  foundationYear2: 1173,
-  mainParty: "Striezelmarkt e Bunte Republik Neustadt",
-};
-const city3 = {
-  nameCity3: "Nürnberg".toUpperCase(),
-  stade3: "Bavária",
-  quantity3: Number(`518.365`),
-  capital3: false,
-  foundationYear3: 1040,
-  mainParty: " Nürnberg Christkindlesmarkt e  Classic Open Air",
-};
+let city = [
+  {
+    nameCity: `Weimar`.toUpperCase(),
+    stade: "Turíngia",
+    quantity: Number(`65.004`),
+    capital: false,
+    foundationYear: 1918,
+    mainParty: "Zwiebelfest, Weimarer Weihnacht",
+  },
+  {
+    nameCity: "Dresden".toUpperCase(),
+    stade: "Saxônia",
+    quantity: Number(`554.649`),
+    capital: true,
+    foundationYear: 1173,
+    mainParty: "Striezelmarkt, Bunte Republik Neustadt",
+  },
+  {
+    nameCity: "Nürnberg".toUpperCase(),
+    stade: "Bavária",
+    quantity: Number(`518.365`),
+    capital: false,
+    foundationYear: 1040,
+    mainParty: " Nürnberg Christkindlesmarkt , Classic Open Air",
+  },
+];
 
-const averegePopulation =
+for (cities of city) {
+  for (propriedade in cities) {
+    console.log(`${propriedade}: ${cities[propriedade]}`);
+  }
+  console.log("----------------------");
+}
+
+function imprimirRelatorio(objeto) {
+  for (let elemento in city);
+}
+
+console.log(imprimirRelatorio(city));
+
+/*const averegePopulation =
   (city1.quantity1 + city2.quantity2 + city3.quantity3) / 3;
 
 city1.touristAttraction1 = "Buchenwald Memorial";
@@ -33,7 +48,7 @@ city2.touristAttraction2 = "The Dresden Zwinger";
 city3.touristAttraction3 = "Altstadt";
 const germanCities = [];
 
-if (city1.capital1 === true) {
+if (city[0].capital1 === true) {
   console.log(germanCities.push(city1));
 } else {
   alert(`Weimar não é capital do estado da Turíngia `);
@@ -53,10 +68,9 @@ if (city3.capital3 === true) {
   alert("Nürnberg não é capital do estado da Bavária");
 }
 
-console.table(city1);
-console.table(city2);
-console.table(city3);
+
 
 console.log(
   `População média destas cidades é de  ${averegePopulation.toFixed(3)}`
 );
+*/
